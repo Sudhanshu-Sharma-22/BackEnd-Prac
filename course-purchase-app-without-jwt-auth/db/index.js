c1f61272bce04c014a822cad1f596b439b0d4a1a
Sudhanshu-Sharma-22 <104524090+Sudhanshu-Sharma-22@users.noreply.github.com>
2025-07-01 22:59:29 +0530

    Update index.js

--- course-purchase-app-without-jwt-auth/db/index.js
+++ course-purchase-app-without-jwt-auth/db/index.js
@@ -1,1 +1,42 @@
+const mongoose = require('mongoose');
+const { title } = require('process');
+require('dotenv').config();
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    // Schema definition here
+    username:String,
+    password:String
+});
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username:String,
+    password:String,
+    purchasedCourses:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Course'
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    // Schema definition here
+    title:String,
+    description:String,
+    image:String,
+    price:Number
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
